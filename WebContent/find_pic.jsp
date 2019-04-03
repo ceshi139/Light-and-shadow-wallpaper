0db21dd0c5607e773565ba094875ba8698d29898
Yuanxiaokang <47966442+Yuanxiaokang@users.noreply.github.com>
2019-04-03 10:37:20 +0800

    最新

---
+++ WebContent/find_pic.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <script src="https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js">
+  </script>
+  <script>
+
+  </script>
+  <style type="text/css">
+
+  </style>
+</head>
+
+<body>
+	
+
+</body>
+</html>
---
+++ src/org/bigjava/entity/Type.java
@@ -0,0 +1,37 @@
+package org.bigjava.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Type implements Serializable {
+	private int id;
+	private String picturetype;
+	private Set<Picture> set = new HashSet<Picture>();
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Set<Picture> getSet() {
+		return set;
+	}
+	public void setSet(Set<Picture> set) {
+		this.set = set;
+	}
+	public String getPicturetype() {
+		return picturetype;
+	}
+	public void setPicturetype(String picturetype) {
+		this.picturetype = picturetype;
+	}
+	@Override
+	public String toString() {
+		return "Type [id=" + id + ", picturetype=" + picturetype ;
+	}
+
+	
+	
+}
